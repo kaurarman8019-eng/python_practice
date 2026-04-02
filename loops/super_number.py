8834f1e3ca7e0317a24740b13e6c27839509cd13
Armanpreet Kaur <kaurarman8019@gmail.com>
2026-04-02 11:35:53 +0530

    Added super number program in loops

---
+++ loops/super_number.py
@@ -0,0 +1,17 @@
+num = int(input("enter number: "))
+temp = num
+sum = 0
+
+while temp > 0:
+    digit = temp % 10
+    fact = 1
+    for i in range(1,digit+1):
+        fact *= i
+        sum += fact
+        temp //= 10
+    
+if sum == num:
+    print("Strong number")
+    
+else:
+    print("Not a strong number")
